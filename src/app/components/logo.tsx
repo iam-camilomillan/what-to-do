827e54fe06c0e7714d1779c6ce4023954de28f78
iam-camilomillan <iam.camilomillan@gmail.com>
2023-10-17 13:54:52 -0500

    Clean Next.js project.

--- src/app/components/logo.tsx
+++
@@ -1,9 +0,0 @@
-export default function () {
-  return (
-    <>
-      <span className="text-purple-300">What</span>
-      <span className="text-cyan-300">To</span>
-      <span className="text-red-300">Do</span>
-    </>
-  );
-}
