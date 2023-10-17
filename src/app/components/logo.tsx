e2030934402595a6b5bc110ea060eb0b1d638adf
iam-camilomillan <iam.camilomillan@gmail.com>
2023-10-17 16:01:07 -0500

    Home added.

---
+++ src/app/components/logo.tsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <>
+      <span className="text-purple-300">What</span>
+      <span className="text-cyan-300">To</span>
+      <span className="text-red-300">Do</span>
+    </>
+  );
+}
